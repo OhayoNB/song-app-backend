5b464830019d6e32470aeaaeeef0524807d4c0f8
ohayonb <ohayonzzb@gmail.com>
2023-09-05 10:40:56 +0300

    edited app module to add postgres created song module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { SongModule } from './song/song.module';
 
 @Module({
-  imports: [],
+  imports: [SequelizeModule.forRoot({
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'b260292o',
+    database: 'postgres',
+    autoLoadModels: true,
+    synchronize: true,
+  }), SongModule],
   controllers: [AppController],
   providers: [AppService],
 })
